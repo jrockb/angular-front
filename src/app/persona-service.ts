208bd02b759171fddfadbbab56ca9f95293ab51f
jrockb87 <jrockb@gmail.com>
2021-02-09 19:31:54 -0500

    agregada clase PersonaService

---
+++ src/app/persona-service.ts
@@ -0,0 +1,49 @@
+import { DataService } from './data-service';
+import { Persona } from './persona.model';
+import { Injectable } from '@angular/core';
+
+/**
+ * Administrar el arreglo de personas que se va a utilizar para desplegar esta información
+ * en la vista de Angular
+ */
+@Injectable() // esta clase será un servicio
+export class PersonaService {
+
+  personas: Persona[] = []; // arreglo inicializado con 0 elementos
+
+  constructor( private dataService: DataService) {} // ya se puede acceder al servicio dataservice
+
+  // se usa para modificar el valor del arreglo debido a la llamada asincrona
+  setPersonas(personas: Persona[]) {
+    this.personas = personas;
+  }
+
+  agregarPersona(persona: Persona) {
+    console.log('Persona a agregar: ' + persona.nombre );
+    this.dataService.agregarPersonas(persona).subscribe(
+      (persona: Persona) => { // recuperamos el objeto persona con el id persona recien agregado
+        console.log('Se agrega al arreglo la persona recien insertada subscriber: ' + persona.idPersona);
+        this.personas.push(persona);
+      });
+  }
+
+  encontrarPersonas(id: number) {
+    const persona: Persona = this.personas.find(persona => persona.idPersona == id);
+    console.log('persona encontrada: ' + persona.idPersona + ' ' + persona.nombre );
+    return persona;
+  }
+
+  modificarPersona(id: number, persona: Persona){
+    console.log('persona a modificar: ' + persona.idPersona);
+    this.dataService.modificarPersona(id, persona);
+  }
+
+  eliminarPersona(id: number){
+    console.log('eliminar persona con id: ' + id);
+    // se necesita eliminar el objeto persona del arreglo y del objeto recibido
+    const index = this.personas.findIndex(persona => persona.idPersona == id);
+    this.personas.splice(index,1);
+    this.dataService.eliminarPersona(id);
+  }
+
+}
